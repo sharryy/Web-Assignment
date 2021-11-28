a72385e94659b501850f2b9f7c81219c9578f2c4
Sharryy <ibneadam388@gmail.com>
2021-11-28 19:42:41 +0500

    Changes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const initBg = (autoplay = true) => {
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
 
-    if(!autoplay) {
-      $.backstretch('pause');  
-    }    
+    if (!autoplay) {
+        $.backstretch('pause');
+    }
 }
 
 const setBg = id => {
@@ -20,36 +20,36 @@ const setBgOverlay = () => {
 
     $('.tm-bg').height(bgHeight);
 
-    if(windowWidth > 768) {
+    if (windowWidth > 768) {
         tmBgLeft.css('border-left', `0`)
-                .css('border-top', `${bgHeight}px solid transparent`);                
+            .css('border-top', `${bgHeight}px solid transparent`);
     } else {
         tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
-                .css('border-top', `0`);
+            .css('border-top', `0`);
     }
 }
 
 $(document).ready(function () {
     const autoplayBg = true;	// set Auto Play for Background Images
-    initBg(autoplayBg);    
+    initBg(autoplayBg);
     setBgOverlay();
 
-    const bgControl = $('.tm-bg-control');            
-    bgControl.click(function() {
+    const bgControl = $('.tm-bg-control');
+    bgControl.click(function () {
         bgControl.removeClass('active');
         $(this).addClass('active');
-        const id = $(this).data('id');                
+        const id = $(this).data('id');
         setBg(id);
     });
 
-    $(window).on("backstretch.after", function (e, instance, index) {        
+    $(window).on("backstretch.after", function (e, instance, index) {
         const bgControl = $('.tm-bg-control');
         bgControl.removeClass('active');
-        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);
         current.addClass('active');
     });
 
-    $(window).resize(function() {
+    $(window).resize(function () {
         setBgOverlay();
     });
 });
